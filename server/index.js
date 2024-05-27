ebe4f52908c725d5154d9c301268bd58bafd2a3d
Chaitali Patidar <99090643+chaitalipatidar2003@users.noreply.github.com>
2024-05-27 16:37:44 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,13 +6,15 @@ import dotenv from 'dotenv';
 import Connection from './database/db.js';
 import Routes from './routes/Routes.js';
 
+const path = requre("path") ;
+
 
 dotenv.config();
 const app = express();
 
 const PORT = 8000;
 
-// const _dirname= path.resolve();
+const _dirname= path.resolve();
 
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
@@ -26,8 +28,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use('/', Routes);
 
-// app.use(express.static(path.join(_dirname,"/client/dist")));
+app.use(express.static(path.join(_dirname,"/client/dist")));
 
-// app.get("", (req,res)=>{
-//     res.sendFile(path.join(_dirname,"client","dist","index.html"));
-// })
+ app.get("", (req,res)=>{
+     res.sendFile(path.join(_dirname,"client","dist","index.html"));
+ })
