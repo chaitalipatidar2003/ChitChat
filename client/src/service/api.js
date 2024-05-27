521de7b3c094d0f77370c6c435b16b745f1ad19e
Chaitali Patidar <99090643+chaitalipatidar2003@users.noreply.github.com>
2024-05-27 20:19:46 +0530

    Update api.js

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,7 +1,7 @@
   
 import axios from 'axios';
 
-const url = 'https://chitchat-bjfe.onrender.com/';
+const url = 'https://chitchat-bjfe.onrender.com';
 
 export const addUser = async (data) => {
     try {
